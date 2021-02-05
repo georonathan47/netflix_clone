b396ce3e5896ca8aca6efc2ac5f68c974e95c6ab
Aaron-Wright <georonathan47@gmail.com>
2021-02-05 11:38:55 +0000

    banner + nav screens

--- src/Nav.js
+++ src/Nav.js
@@ -1,23 +1,40 @@
-import React from 'react'
-import "./nav.css";
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
 
-function nav() {
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.screenY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
   return (
-    <div className="nav">
-    <img 
-    className="nav_logo"
-      src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
-      alt="logo" 
-    />
-    <img 
-    className="Nav_avatar"
-      src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-      alt="avatar"
-    />
-    <h1> The NavBar</h1>
-      
+    <div className= {`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="logo"
+        />
+
+        <img
+          className="nav_avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn.png"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default nav
+export default Nav;
